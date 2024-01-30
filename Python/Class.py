3725369a64cb4a1365ea1328a0a1e8688443cc8b
Mubashir Ali <jamalimubashirali@gmail.com>
2024-01-30 23:47:49 +0500

    Hello

--- Python/Class.py
+++ Python/Class.py
@@ -1,8 +1,10 @@
 class myclass:
     a = "Mubashir Ali"
     b = "Jamali"
+    c = "is Software Engineer"
     def myfun(self):
         print(self.a)
         print(self.b)
+        print(self.c)
 m = myclass()
 m.myfun()
