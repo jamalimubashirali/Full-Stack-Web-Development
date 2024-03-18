698be36ec3e95cc3066c0050ff82541ff8e0b890
Mubashir Ali <21sw071@students.muet.edu.pk>
2024-03-18 15:25:47 +0500

    How to make a request in using https module

---
+++ Html,CSS,JavaScript/Making_request.js
@@ -0,0 +1,10 @@
+const { get } = require('https');
+
+get('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(chunk);
+    });
+    res.on('end', () => {
+        console.log("Process ends");
+    });
+});
