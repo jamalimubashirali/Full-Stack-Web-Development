1260a4483d061c9ecb17a1fe046bbb27f2d1c275
Mubashir Ali <21sw071@students.muet.edu.pk>
2024-12-09 22:37:14 +0500

    Confugered the Cloudnary service for file uploads

---
+++ Node js/Practice 1/src/utils/cloudnary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECERT// Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath)
+            return null;
+        const response = await cloudinary.uploader.upload(localFilePath , {
+            resource_type : "auto"
+        });
+
+        // 
+        console.log(`File uploaded sucessfully ${response.url}`)
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        // If upload fails this removes the file
+        return null;
+    }
+}
+
+
+// To upload an image
+await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
